346ff0ba22754ade9dd680b7fb98520e857e1cb7
Harshita Mathur <mail2harshitamathur@gmail.com>
2025-10-26 06:51:44 +0000

    Add files via upload

---
+++ EX1q4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+int a, b , Sum ;
+printf("Enter the first number (a):");
+scanf ("%d" , &a );
+printf ("Enter the second number (b):");
+scanf ("%d" , &b );
+
+Sum = (a+b);
+printf("The Sum is %d" , Sum );
+return 0;
+}
